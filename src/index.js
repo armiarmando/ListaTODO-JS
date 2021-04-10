9ded16d28c53670a139f95445a277bb1f23acf27
Armando <gutierrezmartinezjosearmando@gmail.com>
2021-04-09 21:39:35 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+import { Todo, TodoList} from './classes/index.js';
+import { crearTodoHtml } from './js/componentes';
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
+console.log('todos',todoList.todos);
